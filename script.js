2cbc044db5ba23e6ae274f31b98b4bb47706ef15
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2019-11-06 19:15:17 -0600

    finished the studio, trying bonus now

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        console.log(response);
+
+        response.json().then(function(json) {
+            console.log(json);
+
+            for (let i=0; i < json.length; i++){
+                const astronaut = document.getElementById("container");
+                astronaut.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                <li>Active: ${json[i].active}</li>
+                                <li>Skills: ${json[i].skills}</li>
+                            </ul>
+                    </div>
+                    <img class="avatar" src="${json[i].picture}">
+                </div>
+                `;
+            }
+        
+            //Bonus 1
+
+            //Bonus 2
+
+            //Bonus 3
+        
+        });
+    });
+ });
